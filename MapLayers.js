4e1ac6f9ce0fbf393846033304c9b36f09682f3e
Matthew Plummer <Labeaux@Matthews-MacBook-Air.local>
2018-11-12 11:16:29 -0500

    Made some changes

--- MapLayers.js
+++ MapLayers.js
@@ -1,63 +1,58 @@
- mapboxgl.accessToken = 'pk.eyJ1Ijoiam1rNjQzNiIsImEiOiJjamRtM2U0bDQwZ2RnMnFvNHluMWR2Zjg1In0.9-Q_u-oGKWoq54QDssVViQ';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/light-v9',
-            center: [-75.3, 40.0],
-            zoom: 11,
-            width: 300,
-            height: 300
-        });
+map.on('load', function () {
 
-        map.on('load', function () {
+    map.addSource('Airports', {
+        type: 'geojson',
+        data: "https://github.com/matthew-plummer/AirWhere/blob/master/usa_airports.geojson"
+    });
 
-            map.addLayer({
-                "id": "United1",
-                "type": "circle",
-                "source": {
-                    "type": "geojson",
-                    "data": bikeThefts
-                },
+    map.addLayer({
+        "id": "United1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
 
-            });
+    });
 
-            map.setFilter('United1', ['==', 'UCR', '625'])
+    map.setFilter('United1', ['==', 'UCR', '625'])
 
 
-            map.addLayer({
-                "id": "Southwest1",
-                "type": "circle",
-                "source": {
-                    "type": "geojson",
-                    "data": bikeThefts
-                },
+    map.addLayer({
+        "id": "Southwest1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
 
-            });
+    });
 
-            map.setFilter('Southwest1', ['==', 'UCR', '615'])
-
-
-            map.addLayer({
-                "id": "Delta1",
-                "type": "circle",
-                "source": {
-                    "type": "geojson",
-                    "data": bikeThefts
-                },
-
-            });
-
-            map.setFilter('Delta1', ['==', 'UCR', '635'])
+    map.setFilter('Southwest1', ['==', 'UCR', '615'])
 
 
-            map.addLayer({
-                "id": "American1",
-                "type": "circle",
-                "source": {
-                    "type": "geojson",
-                    "data": bikeThefts
-                },
+    map.addLayer({
+        "id": "Delta1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
 
-            });
+    });
 
-            map.setFilter('American1', ['==', 'UCR', '645'])
-        });
+    map.setFilter('Delta1', ['==', 'UCR', '635'])
+
+
+    map.addLayer({
+        "id": "American1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
+
+    });
+
+    map.setFilter('American1', ['==', 'UCR', '645'])
+});
